50e721d4b6ccdd04caf47779e9e8a990ec7a62c2
Jorge Cano <jacano.sosa@gmail.com>
2017-05-03 15:29:05 -0500

    Funciones para el manejo de la elección de una imagen de usuario

--- app/src/main/java/com/uady/proyectofinal/MainActivity.java
+++ app/src/main/java/com/uady/proyectofinal/MainActivity.java
@@ -1,13 +1,74 @@
 package com.uady.proyectofinal;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int PICK_IMAGE_REQUEST = 1;
+
+    Bitmap userImage = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void registerNewDeliveryMan(){
+
+        //leer datos
+        //String JSONPayload = EntityFormatter.getInstance().deliveryToJSON();
+        //new HTTPPostAsyncTask(JSONPayload).execute(url);
+    }
+
+    public void pickAnImage(View v){
+
+        Intent intent = new Intent();
+        // Show only images, no videos or anything else
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        // Always show the chooser (if there are multiple options available)
+        startActivityForResult(Intent.createChooser(intent, "Elige tu foto"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                userImage = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                //ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                //imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String imageToBase64String(Bitmap Image){
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        Image.compress(Bitmap.CompressFormat.JPEG, 100, baos); //bm is the bitmap object
+        byte[] imageBytes = baos.toByteArray();
+        Base64.encodeToString(imageBytes, Base64.DEFAULT);
     }
 }
