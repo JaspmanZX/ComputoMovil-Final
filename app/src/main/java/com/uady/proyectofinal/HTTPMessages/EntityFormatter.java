27e5ceedce7099724e02b784ee147ce9df7d06b8
Soto <jose44_sp@hotmail.com>
2017-05-03 15:32:49 -0500

    Añadido clase para realizar el post, y el Formatter de JSON

---
+++ app/src/main/java/com/uady/proyectofinal/HTTPMessages/EntityFormatter.java
@@ -0,0 +1,54 @@
+package com.uady.proyectofinal.HTTPMessages;
+
+/**
+ * Created by José on 03/05/2017.
+ */
+
+public class EntityFormatter {
+
+    private EntityFormatter entityFormatter = new EntityFormatter();
+
+    private EntityFormatter(){
+        ;
+    }
+
+    public EntityFormatter getEntityFormatter(){
+
+        return entityFormatter;
+    }
+
+    public String deliveryToJSON(
+            String name,
+            String email,
+            String password,
+            String lastName1,
+            String lastName2,
+            String birthday,
+            String phone,
+            String lat,
+            String lon,
+            String base64
+    ){
+
+        String entityFormat =
+                "{\n" +
+                        "  \"name\": \""+name+"\",\n" +
+                        "  \"email\": \""+email+"\",\n" +
+                        "  \"password\": \""+password+"\",\n" +
+                        "  \"lastName1\": \""+lastName1+"\",\n" +
+                        "  \"lastName2\": \""+lastName2+"\",\n" +
+                        "  \"birthday\": \""+birthday+"\",\n" +
+                        "  \"phone\": \""+phone+"\",\n" +
+                        "  \"location\": {\n" +
+                        "    \"lat\": "+lat+",\n" +
+                        "    \"lon\": "+lon+"\n" +
+                        "  },\n" +
+                        "  \"photo\": {\n" +
+                        "     \"base64\":\""+base64+"\",\n" +
+                        "    \"extension\": \"jpg\"\n" +
+                        "  }\n" +
+                        "}";
+
+        return entityFormat;
+    }
+}
---
+++ app/src/main/java/com/uady/proyectofinal/HTTPMessages/HttpPostAsyncTask.java
@@ -0,0 +1,56 @@
+package com.uady.proyectofinal.HTTPMessages;
+
+import android.os.AsyncTask;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by José on 03/05/2017.
+ */
+
+public class HttpPostAsyncTask extends AsyncTask{
+
+    private String entity;
+
+    public HttpPostAsyncTask( String entity ){
+
+        this.entity = entity;
+    }
+
+    public void POST(String url){
+
+        HttpPost post = new HttpPost(url);
+        post.addHeader("Content-Type", "application/json");
+
+        StringEntity entity = null;
+
+        try {
+            entity = new StringEntity( this.entity );
+            post.setEntity( entity );
+            HttpClient client = new DefaultHttpClient();
+            HttpResponse response = null;
+            response = client.execute(post);
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
