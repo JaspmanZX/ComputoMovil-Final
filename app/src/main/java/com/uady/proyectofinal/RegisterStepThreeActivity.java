0c31c120460349b515c32c239baa0646e0d59c68
Soto <jose44_sp@hotmail.com>
2017-05-09 10:29:47 -0500

    Agregado código de API de Google para el mapa de la Actividad 3

---
+++ app/src/main/java/com/uady/proyectofinal/RegisterStepThreeActivity.java
@@ -0,0 +1,19 @@
+package com.uady.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+/**
+ * Created by José on 09/05/2017.
+ */
+
+public class RegisterStepThreeActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
