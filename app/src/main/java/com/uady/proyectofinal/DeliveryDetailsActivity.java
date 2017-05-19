e6f5d1840bb4487e18f02c23966cae296815e92b
Jorge Cano <jacano.sosa@gmail.com>
2017-05-19 11:04:28 -0500

    Creación de la tarea asíncrona que maneja la obtención de detalles del pedido

---
+++ app/src/main/java/com/uady/proyectofinal/DeliveryDetailsActivity.java
@@ -0,0 +1,25 @@
+package com.uady.proyectofinal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.uady.proyectofinal.HTTPMessages.GETDeliveryDetails;
+
+public class DeliveryDetailsActivity extends AppCompatActivity {
+
+    //private Delivery delivery;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int delivery_id = 5; //eventualmente se leerá del intent para obtener la id del delivery elegido
+        new GETDeliveryDetails(this).execute("http://69.46.5.165:8081/dlv1601/public/api/deliveryman/delivery/" + delivery_id);
+    }
+
+    public void displayDeliveryDetalis(){
+
+        //this.delivery = delivery;
+    }
+}
---
+++ app/src/main/java/com/uady/proyectofinal/HTTPMessages/GETDeliveryDetails.java
@@ -0,0 +1,79 @@
+package com.uady.proyectofinal.HTTPMessages;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.uady.proyectofinal.DeliveryDetailsActivity;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by jorge on 19/05/17.
+ */
+
+public class GETDeliveryDetails extends AsyncTask<String, Void, String> {
+
+    DeliveryDetailsActivity activity;
+
+    public GETDeliveryDetails(DeliveryDetailsActivity activity) {
+        this.activity = activity;
+    }
+
+    @Override
+    protected String doInBackground(String... urls) {
+        return GET(urls[0]);
+    }
+
+    // onPostExecute despliega el resultado.
+    @Override
+    protected void onPostExecute(String result) {
+
+        if(!result.equals("Problemas!")){
+            System.out.println(result);
+            //se genera el objeto que contiene la info de delivery
+            this.activity.displayDeliveryDetalis();
+        }
+    }
+
+    public static String GET(String url) {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+// HttpClient
+            HttpClient httpclient = new DefaultHttpClient();
+// GET operacion
+            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+// recibe respuesta
+            inputStream = httpResponse.getEntity().getContent();
+// procesa resp a string
+            if (inputStream != null)
+                result = convertInputStreamToString(inputStream);
+            else
+                result = "Problemas!";
+        } catch (Exception e) {
+            Log.d("InputStream", e.getLocalizedMessage());
+        }
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+
+        while ((line = bufferedReader.readLine()) != null)
+            result += line;
+
+        inputStream.close();
+        return result;
+    }
+}
